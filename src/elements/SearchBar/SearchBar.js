a1317bf019a2df35bbbf1cae2b842428a7c9f029
koncalovic <milanakoncalovic@hotmail.com>
2019-04-16 22:28:51 +0200

    elements-js part 1

---
+++ src/elements/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesme';
+import './SearchBar';
+
+class SearchBar extends Component {
+	state = {
+
+	}
+
+	render() {
+		return(
+			<div>
+				SearchBar
+			</div>
+		)
+	}
+	
+}
+
+export default SearchBar;
