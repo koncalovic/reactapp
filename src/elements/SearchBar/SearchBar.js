a535d32b71f4e2bfd86768eebe03dc895429db53
koncalovic <milanakoncalovic@hotmail.com>
2019-04-17 21:16:41 +0200

    Home component added

--- src/elements/SearchBar/SearchBar.js
+++ src/elements/SearchBar/SearchBar.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesme';
-import './SearchBar';
+import {FaSearch} from 'react-icons/fa';
+import './SearchBar.css';
 
 class SearchBar extends Component {
 	state = {
+		value: ''
+	}
 
+	timeout = null;
+
+	doSearch = (event) => {
+		this.setState({ value: event.target.value })
+		clearTimeout(this.timeout);
+
+		this.timeout = setTimeout( () => {
+			this.props.callback(this.state.value);
+		}, 500);
 	}
 
 	render() {
 		return(
-			<div>
-				SearchBar
+			<div className="rmdb-searchbar">
+				<div className="rmdb-searchbar-content">
+					<div className="rmdb-fa-search"><FaSearch size="2em" /></div>	
+						<input
+							type="text"
+							className="rmdb-searchbar-input"
+							placeholder="Search"
+							onChange={this.doSearch}
+							value={this.state.value}
+						/>
+					</div>
 			</div>
 		)
 	}
