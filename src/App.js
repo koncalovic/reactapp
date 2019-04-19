5445495e80c6d8bc3740715dc26ac4b9fa1a2ec2
koncalovic <milanakoncalovic@hotmail.com>
2019-04-19 15:16:40 +0200

    Router, Links

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Header from './elements/Header/Header';
 import Home from './Home/Home';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie/Movie';
+import NotFound from './elements/NotFound/NotFound';
 
-class App extends Component {
-  render() {
-    return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
-      <div>
-        <Header />
-        <Home />
-      </div>
-    );
-  }
+const App = () => {
+	return (
+		<Router>
+			<React.Fragment>
+				<Header />
+				<Switch>
+					<Route path="/" exact component={Home}  />
+					<Route path="/:movieId" exact component={Movie}  />
+					<Route component={NotFound} />
+				</Switch>
+			</React.Fragment>
+		</Router>
+	)
 }
 
 export default App;
---
+++ src/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Whooooops. This page dosen't exist</h1>
+    </div>
+  )
+}
+
+export default NotFound;
