360efbff8ee851cbf6731c31d97a0f7d3004ca5a
koncalovic <milanakoncalovic@hotmail.com>
2019-04-24 16:09:56 +0200

    elements-actor, movieInfo and miBar, Navigation

---
+++ src/elements/MovieInfo/MovieInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
+import FontAwesome from '@fortawesome/react-fontawesome';
+import MovieThumb from '../MovieThumb/MovieThumb';
+import './MovieInfo.css';
+
+const MovieInfo = (props) => {
+  return (
+    <div>
+      MovieInfo
+    </div>
+  )
+}
+
+export default MovieInfo;
---
+++ src/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FontAwesome from '@fortawesome/react-fontawesome';
+import { calcTime, convertMoney } from '../../helpers';
+import './MovieInfoBar.css';
+
+const MovieInfoBar = (props) => {
+  return (
+    <div>
+      MovieInfoBar
+    </div>
+  )
+}
+
+export default MovieInfoBar;
