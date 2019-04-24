7e5d6e86142bdce3820efa17d9f9a3234fe70c7e
koncalovic <milanakoncalovic@hotmail.com>
2019-04-24 23:32:10 +0200

    Movie page

--- src/elements/MovieInfoBar/MovieInfoBar.js
+++ src/elements/MovieInfoBar/MovieInfoBar.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import FontAwesome from '@fortawesome/react-fontawesome';
+import { FaClock, FaMoneyBillAlt, FaTicketAlt } from 'react-icons/fa';
 import { calcTime, convertMoney } from '../../helpers';
 import './MovieInfoBar.css';
 
 const MovieInfoBar = (props) => {
   return (
-    <div>
-      MovieInfoBar
+    <div className="rmdb-movieinfobar">
+      <div className="rmdb-movieinfobar-content">
+        <div className="rmdb-movieinfobar-content-col">
+          <FaClock className="fa-time" size="2em" />
+          <span className="rmdb-movieinfobar-info">Running time: {calcTime(props.time)}</span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+          <FaMoneyBillAlt className="fa-budget" size="2em" />
+          <span className="rmdb-movieinfobar-info">Budget: {convertMoney(props.budget)}</span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+          <FaTicketAlt className="fa-revenue" size="2em" />
+          <span className="rmdb-movieinfobar-info">Revenue: {convertMoney(props.revenue)}</span>
+        </div>
+      </div>
     </div>
   )
 }
